0fdde1215891b6d69f3ce74ffdc86de8775d3130
aridoza <aridoza85@gmail.com>
2017-06-01 20:32:38 -0400

    daily algorithms practice

---
+++ June_2017/Jun0117/main.js
@@ -0,0 +1,17 @@
+// Angry Professor\\
+/*
+Q:
+
+class canceled if less than K students present when class starts
+N students total
+T = number of test cases
+each test case has N and K (cancellation threshold)
+negative = arrived early; 0 = still on time; > 0 is minutes late
+YES = class is cancelled (< K students arrived on time or early)
+NO = class not cancelled (at least K students arrived on time or early)
+
+*/
+
+angryProf = (t,nArr,kArr,times) => {
+  // see hackerrank code for this example
+}
