1a43fe558282d801f3cc0d3d27a6656de3cb38dd
aridoza <aridoza85@gmail.com>
2017-06-06 23:17:07 -0400

    daily algorithms practice

--- June_2017/Jun0517/main.js
+++ June_2017/Jun0517/main.js
@@ -1,17 +1,24 @@
 strangeAdvertising = (n) => {
   let ads = 5;
 
-  let likedAds = Math.floor(ads / 2);
-  let deletedAds = ads - Math.floor(ads / 2);
+  let likedAds = 0;
+  let deletedAds = 0;
+  let likedTotal = 0;
+
+
 
   let nextDay = Math.floor(ads / 2) * 3;
 
   for (let i = 0; i < n; i ++) {
+    likedAds = Math.floor(ads / 2);
+    deletedAds = ads - Math.floor(ads / 2);
+    likedTotal += likedAds;
     nextDay = Math.floor(ads / 2) * 3;
     ads = nextDay;
   }
 
-  console.log(ads);
+  console.log(likedTotal);
 }
 
 // unsuccessful - need to tweak loop depending on size of n
+// update 6/6: Successfully submitted
